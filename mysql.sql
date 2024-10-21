7cfb4f98f21541b962fcda342d55e5b1ad78bd64
Snoopy <huaiping@live.cn>
2024-10-21 21:43:40 +0800

    updated

---
+++ mysql.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `students` (
+  `xh` varchar(6) NOT NULL,
+  `xm` varchar(16) NOT NULL,
+  `xb` varchar(4) NOT NULL,
+  `sfzh` varchar(24) NOT NULL,
+  `fqxm` varchar(16) NOT NULL,
+  `fqdh` varchar(11) NOT NULL,
+  `mqxm` varchar(16) NOT NULL,
+  `mqdh` varchar(11) NOT NULL,
+  `jtzz` varchar(120) NOT NULL,
+  `zkf` varchar(60) NOT NULL,
+  `czbyxx` varchar(60) NOT NULL,
+  `gqty` varchar(12) NOT NULL,
+  `qsh` varchar(30) NOT NULL,
+  `bz` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
