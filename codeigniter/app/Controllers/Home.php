d375afc5156de35e2ff41d5b0278702702414298
Snoopy <huaiping@live.cn>
2024-11-23 21:36:47 +0800

    Logout changed

--- codeigniter/app/Controllers/Home.php
+++ codeigniter/app/Controllers/Home.php
@@ -8,4 +8,11 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function logout()
+    {
+        $session = session();
+        $session->destroy();
+        return redirect()->to(base_url('codeigniter/'));
+    }
 }
--- codeigniter/app/Views/login.php
+++ codeigniter/app/Views/login.php
@@ -14,21 +14,21 @@
 <body>
 <nav class="navbar navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
     <div class="container-fluid">
-        <div class="navbar-brand"><i class="bi bi-database"></i> 综合信息服务管理平台（测试版）</div>
+        <div class="navbar-brand"><i class="bi bi-database"></i> 综合信息服务管理平台</div>
     </div>
 </nav>
 <div class="m-5">
-<form>
-  <div class="col-md-3 offset-md-4">
-    <label for="telephone" class="form-label">账号</label>
-    <input type="telephone" class="form-control form-control-lg" id="telephone" placeholder="请输入账号">
-  </div>
-  <div class="col-md-3 offset-md-4">
-    <label for="password" class="form-label">密码</label>
-    <input type="password" class="form-control form-control-lg" id="password" placeholder="请输入密码">
-  </div>
-  <button type="submit" class="btn btn-primary btn-lg col-md-3 offset-md-4" style="margin-top:20px;">登录</button>
-</form>
+    <form>
+        <div class="col-md-3 offset-md-4">
+            <label for="telephone" class="form-label">账号</label>
+            <input type="telephone" class="form-control form-control-lg" id="telephone" placeholder="请输入账号">
+        </div>
+        <div class="col-md-3 offset-md-4">
+            <label for="password" class="form-label">密码</label>
+            <input type="password" class="form-control form-control-lg" id="password" placeholder="请输入密码">
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg col-md-3 offset-md-4" style="margin-top:20px;">登录</button>
+    </form>
 </div>
 <script src="https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.min.js"></script>
